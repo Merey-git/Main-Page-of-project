8a37d92c98be3a21b5fd2bf88ba60995d7edb0b5
Merey-git <merei.kuanyshkhan@narxoz.kz>
2025-03-17 02:04:19 +0500

    Main page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-const MovieCard = ({ title, genre, rating, image }) => (
-    <div className="movie-card">
-      <img src={image} alt={title} className="movie-image" />
-      <h2 className="movie-title">{title}</h2>
-      <p className="movie-genre">{genre}</p>
-      <p className="movie-rating">⭐ {rating}</p>
-      <button className="watch-button">Watch Now</button>
-    </div>
-  );
+const MovieCard = ({ title, genre, rating, image, onWatchClick }) => (
+  <div className="movie-card">
+    <img src={image} alt={title} className="movie-image" />
+    <h2 className="movie-title">{title}</h2>
+    <p className="movie-genre">{genre}</p>
+    <p className="movie-rating">⭐ {rating}</p>
+    <button className="watch-button" onClick={onWatchClick}>Watch Now</button>
+  </div>
+);
 
 export default MovieCard;
+
