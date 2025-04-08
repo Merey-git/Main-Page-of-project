ea1999e60e2283eb4d54174279805ccc04db7a79
Merey-git <merei.kuanyshkhan@narxoz.kz>
2025-04-08 15:53:30 +0500

    project

--- src/components/GenreContext.js
+++ src/components/GenreContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState } from "react";
 
 export const GenreContext = createContext();
-
+ 
 export const GenreProvider = ({ children }) => {
   const [genre, setGenre] = useState("All");
   return (
